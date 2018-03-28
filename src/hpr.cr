24718b1a526dab1ccbec8628fa1199a45814686f
icyleaf <icyleaf.cn@gmail.com>
2018-03-28 18:25:39 +0800

    refactor: rename logger name

--- src/hpr.cr
+++ src/hpr.cr
@@ -2,25 +2,27 @@ require "./hpr/*"
 require "gitlab"
 
 module Hpr
+  extend self
+
   CONFIG_FILE = "config/hpr.json"
 
   @@config : Config | Nil
-  def self.config
+  def config
     @@config ||= Config.load(CONFIG_FILE)
     @@config.not_nil!
   end
 
   @@gitlab : Gitlab::Client | Nil
-  def self.gitlab
+  def gitlab
     @@gitlab ||= Gitlab.client(config.gitlab.endpoint.to_s, config.gitlab.private_token)
     @@gitlab.not_nil!
   end
 
-  @@log_instance : Logger | Nil
-  def self.log
-    @@log_instance ||= Logger.new(STDOUT)
-    @@log_instance.not_nil!.level = Logger::DEBUG
-    @@log_instance.not_nil!.progname = "hpr"
-    @@log_instance.not_nil!
+  @@logger : Logger | Nil
+  def logger
+    @@logger ||= Logger.new(STDOUT)
+    @@logger.not_nil!.level = Logger::DEBUG
+    @@logger.not_nil!.progname = "hpr"
+    @@logger.not_nil!
   end
 end
