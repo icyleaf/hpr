0d8f8467b45382aef800877cd0af0ea5b7848fba
icyleaf <icyleaf.cn@gmail.com>
2018-10-30 18:08:30 +0800

    fix: 更新 gitignore 并添加丢失的 http api

---
+++ src/hpr/apis/layer.cr
@@ -0,0 +1,49 @@
+require "sidekiq/api"
+
+module Hpr::API
+  class Entrance < Salt::App
+    def call(env)
+      {200, {"Content-Type" => "application/json"}, [{message: "welcome to hpr api layer"}.to_json]}
+    end
+  end
+
+  class Info < Salt::App
+    def call(env)
+      stats = Sidekiq::Stats.new
+      scheduled_set = Sidekiq::ScheduledSet.new
+      scheduleds = scheduled_set.each_with_object([] of Hash(String, String)) do |retri, obj|
+        obj << {
+          "name"         => retri.args[0].to_s,
+          "scheduled_at" => retri.at.to_s,
+        }
+      end
+
+      names = CLIENT.list_repositories
+      body = {
+        hpr: {
+          version:      Hpr::VERSION.to_s,
+          repositroies: {
+            total: names.size,
+            entry: names,
+          },
+        },
+        jobs: {
+          total_scheduled: stats.scheduled_size,
+          total_enqueued:  stats.enqueued,
+          total_failures:  stats.failed,
+          total_processed: stats.processed,
+          total_queues:    stats.queues,
+          scheduleds:      scheduleds,
+        },
+      }.to_json
+
+      {200, {"Content-Type" => "application/json"}, [body]}
+    end
+  end
+
+  class NotFound < Salt::App
+    def call(env)
+      {404, {"Content-Type" => "application/json"}, [{messaeg: "Not found api"}.to_json]}
+    end
+  end
+end
