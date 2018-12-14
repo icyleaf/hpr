e3d6f571034ea4afb38c300e9618572b9efe8ff0
icyleaf <icyleaf.cn@gmail.com>
2018-12-14 18:18:53 +0800

    fix: 只有 verbose 模式才显示 sql 语句

--- src/hpr/core/core.cr
+++ src/hpr/core/core.cr
@@ -8,7 +8,7 @@ module Hpr
   macro init(config)
     sqlite_path = Hpr.db_path({{ config.id }}.root_path)
     adapter = Granite::Adapter::Sqlite.new({name: "hpr", url: "sqlite3://#{sqlite_path}"})
-    Granite.settings.logger = Logger.new STDOUT
+    Granite.settings.logger = Logger.new STDOUT if Hpr.verbose?
     Granite.settings.default_timezone = "Asia/Shanghai"
     Granite::Adapters << adapter
     Model::Repository.adapter = adapter
